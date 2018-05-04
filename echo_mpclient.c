1a3914317076eaa010a32d816ba30fe3fd82ad80
wenbin <wenbin996@sina.com>
2018-05-05 00:48:45 +0800

    add echo_mpserv.c and echo_mpclient.c

---
+++ echo_mpclient.c
@@ -0,0 +1,80 @@
+/*回声客户端--基于I/O程序分割*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 30
+void error_handling(char *message);
+void write_routine(int sock, char *buf);
+void read_routine(int sock, char *buf);
+
+int main(int argc, char *argv[])
+{
+	int sock;
+	pid_t pid;
+	char buf[BUF_SIZE];
+	struct sockaddr_in serv_adr;
+	if(argc != 3)
+	{
+		printf("Usage: %s <IP><port>", argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);  /*转为整型且为网络字节序*/
+	serv_adr.sin_port = htons(atoi(argv[2]));  /*htons()主机字节序转网络字节序*/
+
+	if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+		error_handling("connect() error");
+
+	pid = fork();
+
+	//父进程中Pid为子进程文件描述符，子进程中pid为0
+	if(pid == 0)
+		write_routine(sock, buf);
+	else
+		read_routine(sock, buf);
+
+	close(sock);
+	return 0;
+}
+
+void read_routine(int sock, char *buf)
+{
+	while(1)
+	{
+		int str_len = read(sock, buf, BUF_SIZE);
+		if(str_len == 0)
+			return;
+
+		buf[str_len] = 0;
+		printf("Message from server: %s", buf);
+	}
+}
+
+void write_routine(int sock, char *buf)
+{
+	while(1)
+	{
+		fgets(buf, BUF_SIZE, stdin);
+		if(!strcmp(buf, "q\n") || !strcmp(buf, "Q\n"))
+		{
+			shutdown(sock, SHUT_WR);
+			return;
+		}
+		write(sock, buf, strlen(buf));
+	}
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
